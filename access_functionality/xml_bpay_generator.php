ec2be2e6cc9aa2bea518936c4f89a5e35469b180
unknown <turcanualexandru1992@gmail.com>
2017-12-07 11:26:16 +0200

    Test commit from bash
    Just added target blank for bpay_xml_generator

--- access_functionality/xml_bpay_generator.php
+++ access_functionality/xml_bpay_generator.php
@@ -3,7 +3,7 @@
     require "../db.php";
 
     if($_POST['user'] === 'admin'){
-        echo "<a href='../bpay_xml_generator/index.php' id='xml_generator_bpay'>Genereaza Bpay XML</a>";
+        echo "<a href='../bpay_xml_generator/index.php' id='xml_generator_bpay' target='_blank'>Genereaza Bpay XML</a>";
     }
 
 ?>
